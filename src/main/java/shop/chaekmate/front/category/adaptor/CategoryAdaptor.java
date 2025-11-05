f02b9df774d527f13ba327839f9687dfd7541288
chzhxkd100 <chzhxkd100@naver.com>
2025-11-05 20:14:42 +0900

    feat: 카테고리 페이지네이션 처리 추가

--- src/main/java/shop/chaekmate/front/category/adaptor/CategoryAdaptor.java
+++ src/main/java/shop/chaekmate/front/category/adaptor/CategoryAdaptor.java
@@ -2,13 +2,20 @@ package shop.chaekmate.front.category.adaptor;
 
 import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import shop.chaekmate.front.category.dto.response.CategoryHierarchyResponse;
 import shop.chaekmate.front.category.dto.response.CategoryResponse;
 import shop.chaekmate.front.common.CommonResponse;
+import shop.chaekmate.front.category.dto.response.CategoryPageResponse;
 
 @FeignClient(name="category-client" , url = "${chaekmate.gateway.url}")
 public interface CategoryAdaptor {
 
-    @GetMapping("/categories")
+    @GetMapping(value= "/categories")
     CommonResponse<List<CategoryResponse>> getAllCategories();
+
+    @GetMapping(value = "/categories/paged")
+    CommonResponse<CategoryPageResponse<CategoryHierarchyResponse>> getPagedCategories(@RequestParam("page") int page, @RequestParam("size") int size);
 }
--- src/main/java/shop/chaekmate/front/category/service/CategoryService.java
+++ src/main/java/shop/chaekmate/front/category/service/CategoryService.java
@@ -4,8 +4,10 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import shop.chaekmate.front.category.adaptor.CategoryAdaptor;
+import shop.chaekmate.front.category.dto.response.CategoryHierarchyResponse;
 import shop.chaekmate.front.category.dto.response.CategoryResponse;
 import shop.chaekmate.front.common.CommonResponse;
+import shop.chaekmate.front.category.dto.response.CategoryPageResponse;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +18,12 @@ public class CategoryService {
     public List<CategoryResponse> getCategories() {
 
         CommonResponse<List<CategoryResponse>> wrappedResponse = categoryAdaptor.getAllCategories();
+
+        return wrappedResponse.data();
+    }
+
+    public CategoryPageResponse<CategoryHierarchyResponse> getPagedCategories(int page, int size) {
+        CommonResponse<CategoryPageResponse<CategoryHierarchyResponse>> wrappedResponse = categoryAdaptor.getPagedCategories(page, size);
 
         return wrappedResponse.data();
     }
