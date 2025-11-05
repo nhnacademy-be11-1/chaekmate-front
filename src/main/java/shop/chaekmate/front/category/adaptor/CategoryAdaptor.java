1e864235303943d464541ece4d48b50552826dd8
chzhxkd100 <chzhxkd100@naver.com>
2025-11-05 20:28:52 +0900

    feat: 카테고리 삭제 요청 추가

--- src/main/java/shop/chaekmate/front/category/adaptor/CategoryAdaptor.java
+++ src/main/java/shop/chaekmate/front/category/adaptor/CategoryAdaptor.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import shop.chaekmate.front.category.dto.response.CategoryHierarchyResponse;
 import shop.chaekmate.front.category.dto.response.CategoryResponse;
@@ -18,4 +19,7 @@ public interface CategoryAdaptor {
 
     @GetMapping(value = "/categories/paged")
     CommonResponse<CategoryPageResponse<CategoryHierarchyResponse>> getPagedCategories(@RequestParam("page") int page, @RequestParam("size") int size);
+
+    @DeleteMapping(value ="/admin/categories/{id}")
+    CommonResponse<Void> deleteCategory(@PathVariable("id") int id);
 }
