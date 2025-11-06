e63f38b485401e19c9df7a16f879c0078e816b42
chzhxkd100 <42908189+chzhxkd100@users.noreply.github.com>
2025-11-06 21:00:15 +0900

    feat: 관리자 태그 관리 페이지 추가
    * feat: Pageable 사용할 수 있게 추가
    
    * feat: 카테고리 페이지네이션 처리 추가
    
    * feat: 카테고리 관리자 페이지에서 카테고리 페이지네이션
    
    * feat: 카테고리 삭제 버튼 추가
    
    * feat: 카테고리 삭제 요청 추가
    
    * chore: 주석 추가
    
    * feat: 카테고리 삭제 추가
    
    * chore: 주석 추가
    
    * feat: 카테고리 추가 페이지
    
    * feat: 삭제 시 카테고리 캐시 리로딩 추가
    
    * feat: 순환 의존성 해결
    
    * chore: 순서 변경
    
    * feat: 카테고리 직접 추가
    
    * chore: Code smell 제거
    
    * feat: HTML 폼으로 DELETE 요청 보낼 수 있게 설정
    
    * feat: 관리자 카테고리 직접 추가
    
    * feat: code smell 제거
    
    * feat: 태그 관리자 페이지 구현

---
+++ src/main/java/shop/chaekmate/front/tag/adaptor/TagAdaptor.java
@@ -0,0 +1,28 @@
+package shop.chaekmate.front.tag.adaptor;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import shop.chaekmate.front.tag.dto.request.TagCreateRequest;
+import shop.chaekmate.front.tag.dto.response.TagCreateResponse;
+import shop.chaekmate.front.common.CommonResponse;
+import shop.chaekmate.front.tag.dto.response.TagPageResponse;
+import shop.chaekmate.front.tag.dto.response.TagResponse;
+
+@FeignClient(name="tag-client" , url = "${chaekmate.gateway.url}")
+public interface TagAdaptor {
+
+    @GetMapping(value = "/tags/paged")
+    CommonResponse<TagPageResponse<TagResponse>> getPagedTags(@RequestParam("page") int page, @RequestParam("size") int size);
+    
+    @DeleteMapping(value ="/admin/tags/{id}")
+    CommonResponse<Void> deleteTag(@PathVariable("id") Long id);
+
+    @PostMapping(value = "/admin/tags")
+    CommonResponse<TagCreateResponse> createTag(@RequestBody TagCreateRequest request);
+
+}
