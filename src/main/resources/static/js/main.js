1cad20a4b97834a0af06f48e63425be500d5db20
chzhxkd100 <chzhxkd100@naver.com>
2025-11-06 17:02:31 +0900

    chore: code smell 제거

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -92,7 +92,7 @@
             var newVal = parseFloat(oldValue) + 1;
         } else {
             if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
+                newVal = parseFloat(oldValue) - 1;
             } else {
                 newVal = 0;
             }
