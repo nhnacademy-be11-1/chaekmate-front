68dcb47cd93b1af7a8180345d5e0e11baaff5400
chzhxkd100 <chzhxkd100@naver.com>
2025-11-07 17:00:45 +0900

    fix: 충돌해결

--- src/main/java/shop/chaekmate/front/IndexController.java
+++ src/main/java/shop/chaekmate/front/IndexController.java
@@ -1,13 +1,10 @@
 package shop.chaekmate.front;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import shop.chaekmate.front.category.cache.CategoryCache;
 
 @Controller
-@RequiredArgsConstructor
 public class IndexController {
 
     @GetMapping("/")
