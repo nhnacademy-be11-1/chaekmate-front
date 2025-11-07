fa0c34979b52f577a7a1902721bc5f206a6e0fb1
chzhxkd100 <chzhxkd100@naver.com>
2025-11-07 13:12:17 +0900

    feat: 주석 추가

--- src/main/java/shop/chaekmate/front/book/controller/AdminBookController.java
+++ src/main/java/shop/chaekmate/front/book/controller/AdminBookController.java
@@ -18,6 +18,7 @@ public class AdminBookController {
 
     private final AdminBookService bookService;
 
+    // 도서 관리자 페이지
     @GetMapping("/admin/books")
     public String bookManagementView(Model model) {
 
@@ -27,16 +28,19 @@ public class AdminBookController {
         return "admin/book/book-management";
     }
 
+    // 도서 직접 추가 페이지 뷰
     @GetMapping("/admin/books/add-direct")
     public String bookManagementAddDirectView(@ModelAttribute BookCreateRequest request, Model model) {
         return "admin/book/book-management-add-direct";
     }
 
+    // 알라딘 도서 검색 페이지
     @GetMapping("/admin/books/add-aladin")
     public String bookManagementAddAladinView(Model model) {
         return "admin/book/book-management-add-aladin";
     }
 
+    // 도서 추가 요청
     @PostMapping("/admin/books")
     public String createBook(@ModelAttribute BookCreateRequest request) {
 
@@ -44,11 +48,15 @@ public class AdminBookController {
         return "redirect:/admin/books";
     }
 
+    // 도서 상세 페이지
     @GetMapping("/admin/books/{bookId}")
     public String bookManagementDetailView(@PathVariable Long bookId, Model model) {
+
+
         return "admin/book/book-management-detail";
     }
 
+    // 관리자 도서 수정 페이지
     @GetMapping("/admin/books/{bookId}/modify")
     public String bookManagementModifyView(@PathVariable Long bookId, Model model) {
         return "admin/book/book-management-modify";
