8593a1da358d1d09f60d458fdd0f7d4e084a74ad
chzhxkd100 <chzhxkd100@naver.com>
2025-11-07 16:42:25 +0900

    feat: 테스트 프로파일 추가

--- src/test/java/shop/chaekmate/front/FrontApplicationTests.java
+++ src/test/java/shop/chaekmate/front/FrontApplicationTests.java
@@ -2,12 +2,15 @@ package shop.chaekmate.front;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FrontApplicationTests {
 
 	@Test
 	void contextLoads() {
+        // 로드 테스트
 	}
 
 }
