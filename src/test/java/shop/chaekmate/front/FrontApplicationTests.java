8c1fba2aa48e4f6cbe9990fb9ee1687ca11aefe6
chzhxkd100 <chzhxkd100@naver.com>
2025-11-07 17:21:09 +0900

    fix: cicd 수정

--- src/test/java/shop/chaekmate/front/FrontApplicationTests.java
+++ src/test/java/shop/chaekmate/front/FrontApplicationTests.java
@@ -2,11 +2,16 @@ package shop.chaekmate.front;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import shop.chaekmate.front.category.cache.CategoryCache;
 
 @SpringBootTest
 class FrontApplicationTests {
 
-	@Test
+    @MockitoBean
+    private CategoryCache categoryCache;
+
+    @Test
 	void contextLoads() {
         // 로드 테스트
 	}
