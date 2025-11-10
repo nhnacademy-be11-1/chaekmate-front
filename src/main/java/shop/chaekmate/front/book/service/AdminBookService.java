7a1384c837d4861fbd0e918eea015fc6e94d2630
chzhxkd100 <chzhxkd100@naver.com>
2025-11-10 16:10:58 +0900

    feat: 알라딘으로 도서 추가

--- src/main/java/shop/chaekmate/front/book/service/AdminBookService.java
+++ src/main/java/shop/chaekmate/front/book/service/AdminBookService.java
@@ -2,12 +2,15 @@ package shop.chaekmate.front.book.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import shop.chaekmate.front.book.adaptor.AdminBookAdaptor;
 import shop.chaekmate.front.book.dto.request.BookCreateRequest;
 import shop.chaekmate.front.book.dto.request.BookModifyRequest;
 import shop.chaekmate.front.book.dto.response.AdminBookResponse;
 import shop.chaekmate.front.book.dto.response.AdminBookDetail;
+import shop.chaekmate.front.book.dto.response.AladinBookResponse;
 import shop.chaekmate.front.category.service.CategoryService;
 import shop.chaekmate.front.common.CommonResponse;
 import shop.chaekmate.front.tag.service.TagService;
@@ -53,6 +56,15 @@ public class AdminBookService {
     public void deleteBookByBookId(Long bookId){
 
         adminBookAdaptor.deleteBookById(bookId);
+
+    }
+
+    // 알라딘으로 도서 검색
+    public Page<AladinBookResponse> searchBooksByAladin(String query, String searchType, Pageable pageable){
+
+        CommonResponse<Page<AladinBookResponse>> wrappedResponse = adminBookAdaptor.getBooksByAladinSearch(query, searchType, pageable);
+
+        return wrappedResponse.data();
 
     }
 
