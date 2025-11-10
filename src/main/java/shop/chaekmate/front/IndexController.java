c353eded1feab54f6f093e0a546f5a8e8c250c15
chzhxkd100 <42908189+chzhxkd100@users.noreply.github.com>
2025-11-10 19:53:54 +0900

    feat: 프론트 관리자 레이아웃 ,  관리자 메인 페이지 구현
    * fix: 코드스멜제거
    
    * feat: 관리자 레이아웃
    
    * chore: 관리자 레이아웃 적용
    
    * chore: 관리자용 자바스크립트 로딩 제거
    
    * chore: 관리자용 자바스크립트 로딩 추가

--- src/main/java/shop/chaekmate/front/IndexController.java
+++ src/main/java/shop/chaekmate/front/IndexController.java
@@ -12,4 +12,10 @@ public class IndexController {
 
         return "index";
     }
+
+    @GetMapping("/admin")
+    public String adminIndex(Model model){
+
+        return "admin/admin-index";
+    }
 }
