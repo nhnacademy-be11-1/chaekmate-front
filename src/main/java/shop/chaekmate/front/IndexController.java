93bdf356263429ed450c5854d52a1c042081c228
chzhxkd100 <chzhxkd100@naver.com>
2025-11-23 13:41:01 +0900

    fix: 이미지 파일 로딩을 링크로 수정, 이미지 파일 삭제

--- src/main/java/shop/chaekmate/front/IndexController.java
+++ src/main/java/shop/chaekmate/front/IndexController.java
@@ -12,11 +12,14 @@ public class IndexController {
 
         /*
         slide : subtitle, title, description, imageUrl, bookId
-        model.addAttribute("slide1", slide1);
-        model.addAttribute("slide2", slide2);
-        model.addAttribute("slide3", slide3);
+        model.addAttribute("slide1", slide1); 체크메이트추천
+        model.addAttribute("slide2", slide2); 이달의 추천도서
+        model.addAttribute("slide3", slide3); 실시간 조회수 급상승
+        model.addAttribute("banner1", nanner1); 우측 배너1
+        model.addAttribute("banner2", banner2); 우측 배너 2
 
-        dummy book : id, title, author, price, salsePrice, rating, reviewCount, imageUrl
+
+        dummy book : id, title, author, price, salesPrice, rating, reviewCount, imageUrl
         model.addAttribute("recentBooks", dummyBooks);
         model.addAttribute("recommendedBooks", dummyBooks);
         model.addAttribute("mostReviewedBooks", dummyBooks);
