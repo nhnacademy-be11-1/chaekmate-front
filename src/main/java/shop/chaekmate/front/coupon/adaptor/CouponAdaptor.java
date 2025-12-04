0ea7300e4e49d4a2c9e064e584e5006f0fc79df3
Charl-es <124643367+Charl-es@users.noreply.github.com>
2025-12-04 09:54:39 +0900

    Feat/client couponbox (#95)
    * feat: 회원 쿠폰함 구현 및 관리자 쿠폰 적용 도서 검색 구현
    
    * chore: 임포트 추가

--- src/main/java/shop/chaekmate/front/coupon/adaptor/CouponAdaptor.java
+++ src/main/java/shop/chaekmate/front/coupon/adaptor/CouponAdaptor.java
@@ -2,16 +2,19 @@ package shop.chaekmate.front.coupon.adaptor;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
+import shop.chaekmate.front.auth.config.AuthFeignClientConfig;
 import shop.chaekmate.front.common.CommonResponse;
 import shop.chaekmate.front.coupon.dto.request.CouponIssueRequest;
 import shop.chaekmate.front.coupon.dto.response.BookCouponPolicyResponse;
 import shop.chaekmate.front.coupon.dto.response.CouponIssueResponse;
+import shop.chaekmate.front.coupon.dto.response.IssuedCouponResponse;
+import shop.chaekmate.front.coupon.dto.response.UsedCouponResponse;
 
 import java.util.List;
 import shop.chaekmate.front.order.dto.request.BooksAvailableCouponsRequest;
 import shop.chaekmate.front.order.dto.response.BooksAvailableCouponsDetailResponse;
 
-@FeignClient(name = "coupon-client", url = "${chaekmate.gateway.url}")
+@FeignClient(name = "coupon-client", url = "${chaekmate.gateway.url}", configuration = AuthFeignClientConfig.class)
 public interface CouponAdaptor {
 
     @GetMapping("/coupon-policies/books/{bookId}")
@@ -33,4 +36,14 @@ public interface CouponAdaptor {
             @RequestBody BooksAvailableCouponsRequest request
     );
 
+
+    @GetMapping("/issued-coupons/available")
+    CommonResponse<List<IssuedCouponResponse>> getAvailableCoupons(
+            @RequestHeader("X-Member-Id") Long memberId
+    );
+
+    @GetMapping("/issued-coupons/used")
+    CommonResponse<List<UsedCouponResponse>> getUsedCoupons(
+            @RequestHeader("X-Member_Id") Long memberId
+    );
 }
